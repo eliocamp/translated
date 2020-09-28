db5f734f6775d047f0598a105ebaaf42c55a91e7
Elio Campitelli <eliocampitelli@gmail.com>
2020-09-28 17:54:02 -0300

    Corrects alias

--- translate_roclet.R
+++ translate_roclet.R
@@ -10,6 +10,9 @@ roclet_clean.roclet_translate <- function(x, base_path) {
 roclet_process.roclet_translate <- function(x, blocks, env, base_path) {
   blocks <- lapply(blocks, translate_block)
   topic <- roxygen2:::roclet_process.roclet_rd(x, blocks, env, base_path)
+  for (i in seq_along(topic)) {
+    topic[[i]]$sections$alias$value <- topic[[i]]$sections$alias$value[-1]
+  }
 
   files <- vapply(blocks, function(b)  paste0(b$object$alias, ".Rd"), character(1))
   setNames(topic, files)
